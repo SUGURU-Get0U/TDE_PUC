629beb58ba3a705386429ca461de9b6e34fb0a5e
Liam <twich.euedu@gmail.com>
2025-04-02 11:06:51 -0300

    calma vida ta dboa

---
+++ tde3.1/01.py
@@ -0,0 +1,27 @@
+
+# Exercicio 1
+
+"""
+
+Ler do teclado uma quantidade de números inteiros a serem lidos e calcular:
+a) a soma total entre eles
+b) a soma dos que forem pares.
+c) a soma dos que forem ímpares
+d) a amplitude amostral considerando todos os números lidos (diferença entre o maior e o menor)
+
+"""
+
+numKey = []
+
+contNumeros = int(input("digite a quantidade de numeros inteiros a serem adicionados: "))
+
+quantLeituras = 0
+
+while quantLeituras < contNumeros:
+    EntDados = int(input("Digite os números para leitura: "))
+    quantLeituras += 1
+
+
+numeros.append(numKey)
+
+soma = sum(numKey)
